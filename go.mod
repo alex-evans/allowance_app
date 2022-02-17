7b930d2667bf463d309a0ab28d884097af44b129
Alex Evans <alex.evans.d@gmail.com>
2022-02-16 20:42:40 -0600

    It is a start...

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alex-evans/allowance_app
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func homeHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("home.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
+
+	http.ListenAndServe(":5001", r)
+}
